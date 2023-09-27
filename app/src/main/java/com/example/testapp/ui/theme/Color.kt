3ab090e7c295642c4154f0b9d489aae230aab2cd
Sergey <naskar0071@mail.ru>
2023-09-28 01:09:24 +0500

    add files

--- app/src/main/java/com/example/testapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/testapp/ui/theme/Color.kt
@@ -2,10 +2,19 @@ package com.example.testapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val TextPrimary = Color(0xDE000000)
+val TextSecondary = Color(0xFFCCC2DC)
+val TextSecondaryWhite = Color(0x8AFFFFFF)
+val TextDisable = Color(0x61000000)
+val TextSnackbar = Color(0xFFFFFFFF)
+val IconNormal = Color(0xFF97A0AF)
+val IconWhite = Color(0xFFFFFFFF)
+val PrimaryButton = Color(0xFFF15044)
+val PrimaryHighlight = Color(0xFFF88A82)
+val PrimaryDisable = Color(0xFFF5ACA2)
+val AccentNormal = Color(0xFF3695F7)
+val AccentSecondary = Color(0xFFE8F1FA)
+val UICounter = Color(0x1F000000)
+val UIDivider = Color(0x1F000000)
+val BGContainer = Color(0xFFFFFFFF)
+val BackgroundSnackbar = Color(0xFF3D3D4D)
--- app/src/main/java/com/example/testapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/testapp/ui/theme/Type.kt
@@ -2,33 +2,61 @@ package com.example.testapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.testapp.R
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+val Typography: Typography
+    get() = Typography(
+        // Body Bold
+        bodyLarge = TextStyle(
+            fontSize = 14.sp,
+            lineHeight = 18.sp,
+            fontFamily = FontFamily(Font(R.font.fonts)),
+            fontWeight = FontWeight(600),
+        ),
+
+        // Large title
+        titleLarge = TextStyle(
+            fontSize = 34.sp,
+            lineHeight = 41.sp,
+            fontFamily = FontFamily(Font(R.font.fonts)),
+            fontWeight = FontWeight(700),
+            letterSpacing = 0.37.sp,
+        ),
+
+        // Headline Large
+        headlineLarge = TextStyle(
+            fontSize = 24.sp,
+            lineHeight = 28.sp,
+            fontFamily = FontFamily(Font(R.font.fonts)),
+            fontWeight = FontWeight(600),
+        ),
+
+        // Headline
+         headlineMedium = TextStyle(
+            fontSize = 16.sp,
+            lineHeight = 20.sp,
+            fontFamily = FontFamily(Font(R.font.fonts)),
+            fontWeight = FontWeight(600),
+        ),
+
+        // Body
+        bodyMedium = TextStyle(
+            fontSize = 14.sp,
+            lineHeight = 18.sp,
+            fontFamily = FontFamily(Font(R.font.fonts)),
+            fontWeight = FontWeight(400),
+        ),
+
+        // labelSmall
+        labelSmall = TextStyle(
+            fontSize = 12.sp,
+            lineHeight = 16.sp,
+            fontFamily = FontFamily(Font(R.font.fonts)),
+            fontWeight = FontWeight(400),
+        ),
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
-)
